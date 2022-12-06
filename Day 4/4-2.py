f0f126e64139b9b8e728b20866f3695eb6306411
vgoverlord1766 <jgomez@thinkbox.us>
2022-12-05 22:34:29 -0500

    Finished Day 4

--- Day 4/4-2.py
+++ Day 4/4-2.py
@@ -12,8 +12,13 @@ for assignment_pair in assignments:
     second_assignment_start = int(second_assignment.split("-")[0])
     second_assignment_end = int(second_assignment.split("-")[1])
 
-    if (second_assignment_start - first_assignment_start) * (first_assignment_end - second_assignment_end) >= 0:
+    if second_assignment_start <= first_assignment_start <= second_assignment_end:
         assignment_count += 1
-
+    elif second_assignment_start <= first_assignment_end <= second_assignment_end:
+        assignment_count += 1
+    elif first_assignment_start <= second_assignment_start <= first_assignment_end:
+        assignment_count += 1
+    elif first_assignment_start <= second_assignment_end <= first_assignment_end:
+        assignment_count += 1
 
 print(assignment_count)
