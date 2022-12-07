d1a1d71ed2dc06fdf7776b3b8d066e2aaaa32117
vgoverlord1766 <jgomez@thinkbox.us>
2022-12-06 21:51:36 -0500

    Finished 5-1 (Finally)

--- Day 3/3-2.py
+++ Day 3/3-2.py
@@ -1,4 +1,4 @@
-priorities = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+priorities = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "crates", "T", "U", "V", "W", "X", "Y", "Z"]
 total_priority = 0
 with open('rucksacks', 'r') as rucksacks_file:
     rucksacks = rucksacks_file.read().splitlines()
